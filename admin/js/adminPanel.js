7efe932f022a6bc89a0fe8fd81a615b62d304280
Jack Underhill <100008285+Jack-Underhill@users.noreply.github.com>
2025-04-08 13:41:07 -0700

    Admin Basics Designed & Implemented (#36)
    * Admin Basics Implemented
    
    * Removed backup file

---
+++ admin/js/adminPanel.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function ()
+{
+    fetch("/admin/adminPanel.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("adminPanel-placeholder").innerHTML = data;
+
+            adjustSidebarHeight();
+
+            const content = document.querySelector('.admin-content');
+            if(content) {
+                contentObserver.observe(content);
+            }
+        })
+        .catch(error => console.error("Error loading admin panel:", error));
+});
+
+function adjustSidebarHeight() {
+    const sidebar = document.querySelector('.admin-sidebar');
+    const content = document.querySelector('.admin-content');
+
+    sidebar.style.height = 'auto';
+    const contentHeight = content.getBoundingClientRect().height;
+
+    if(contentHeight > 400) {
+        sidebar.style.height = contentHeight + 'px';
+    } else {
+        sidebar.style.height = '400px';
+    }
+}
+
+const contentObserver = new ResizeObserver(() => {
+    adjustSidebarHeight();
+}); 
+
+window.addEventListener('load', adjustSidebarHeight);
+
+window.addEventListener('resize', adjustSidebarHeight);
